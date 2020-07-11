a9a16df5e5abb849cd74dde44bedd8f26cace0e9
satish-rawat <68094515+satish-rawat@users.noreply.github.com>
2020-07-11 21:20:08 +0530

    Update personal-functions.py

--- personal-functions.py
+++ personal-functions.py
@@ -4,14 +4,14 @@ import os
 def path_finder(folder_name=None, search_directory=os.getcwd(), file_type='csv'):
     if folder_name is None:
         file_path = []
-        for r, d, f in os.walk(search_directory):
-            for file in f:
+        for root, directories, files in os.walk(search_directory):
+            for file in files:
                 if file.endswith(file_type):
                     x = file_path.append(os.path.join(r, file))
         return file_path
     else:
-        for r, d, f in os.walk(search_directory):
-            for directory in d:
+        for root, directories, files in os.walk(search_directory):
+            for directory in directories:
                 if directory == folder_name:
                     path = os.path.join(r, directory)
                     return f"Folder Found {path}"
